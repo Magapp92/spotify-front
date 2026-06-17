e21c31308c53e7345d1c7c86c3d384c29f632064
Magaly Puente Parada <magapp92@alumnos.cei.es>
2026-06-18 00:02:44 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Artistas } from './components/Artistas'
+
+const App = () => {
+
+    return (
+        <>
+            <Artistas />
+        </>
+    )
+}
+
+export default App
